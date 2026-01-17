c6519617e60f83bd57fcb1b1c9a8a9e9e1b73ed0
rich-bongo <rich.j.bongo@gmail.com>
2026-01-17 12:56:39 -0500

    fixed logger issue

--- main.py
+++ main.py
@@ -6,27 +6,21 @@ from logging.handlers import TimedRotatingFileHandler
 if __name__ == "__main__":
     logger = logging.getLogger('logger_util')
     logger.setLevel(logging.DEBUG)
-     # create file handler which logs even debug messages
-    fh = logging.FileHandler('log/cat_door.log')
-    fh.setLevel(logging.DEBUG)
-     # create console handler with a higher log level
+   
+    # create file handler which logs even debug messages
+    rollover_handler = TimedRotatingFileHandler('log/cat_door.log', when="midnight", interval=1, backupCount=7)
+    
+     # create formatter and add it to the logger
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s)')
+    logger.addHandler(rollover_handler)
+    rollover_handler.setFormatter(formatter)
+   
+    # create console handler and add handler to logger
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-     # create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
     ch.setFormatter(formatter)
-
-    rollover_handler = TimedRotatingFileHandler(
-    'logger_util',
-    when="midnight",
-    interval=1,
-    backupCount=7
-)
-     # add the handlers to the logger
-    logger.addHandler(fh)
     logger.addHandler(ch)
-    logger.addHandler(rollover_handler)
+    
     current_state = 'Init'
     first_call = True
     state_dict = state.get_class_dict()
